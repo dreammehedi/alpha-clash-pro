358b2b1873c7fa6a10fb3adb7808305d279a23cd
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-02-15 19:28:49 +0600

    This game is fully Complete

---
+++ scripts/playnow.js
@@ -0,0 +1,54 @@
+// play now function
+function playNow() {
+  hide("home-screen");
+  show("play-ground");
+  randomAlphabetCreateAndSet();
+  document.addEventListener("keyup", whatKeyPressedUser);
+}
+// random alphabet create and set
+function randomAlphabetCreateAndSet() {
+  const randomValue = randomAlphabetGenarate();
+  setAlphabetValue("current-alphabet", randomValue);
+  if (randomValue === "/") {
+    setCurrentAlphabetBg("slash");
+  } else {
+    setCurrentAlphabetBg(randomValue);
+  }
+}
+
+// crate what alphabet press user
+function whatKeyPressedUser(event) {
+  // find user what alphabat press
+  const pressKbd = event.key;
+  // user presss esc to show final score page
+  if (pressKbd === "Escape") {
+    hide("play-ground");
+    show("score-screen");
+  }
+  // finde current alphabate
+  const currentAlphabetId = document.querySelector("#current-alphabet");
+  const currentAlphabetValue = currentAlphabetId.innerText;
+  const lowerCurrentAlphabetValue = currentAlphabetValue.toLowerCase();
+  if (pressKbd === lowerCurrentAlphabetValue) {
+    if (currentAlphabetValue === "/") {
+      removeCurrentAlphabetBg("slash");
+    } else {
+      removeCurrentAlphabetBg(lowerCurrentAlphabetValue);
+    }
+    randomAlphabetCreateAndSet();
+    scoreUp("score-up");
+  } else {
+    lifeDown("life-down");
+  }
+}
+// play again function
+function playAgain() {
+  hide("score-screen");
+  show("play-ground");
+  // score value 0
+  const scoreUp = document.getElementById("score-up");
+  scoreUp.innerText = 0;
+  // life set 5
+  const scoreDown = document.getElementById("life-down");
+  scoreDown.innerText = 5;
+}
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,45 +1,67 @@
-function hideElementById(elementId) {
-    const element = document.getElementById(elementId);
-    element.classList.add('hidden');
+//  hide function
+function hide(elementId) {
+  const getElementId = document.querySelector(`#${elementId}`);
+  getElementId.classList.add("hidden");
 }
 
-function showElementById(elementId) {
-    const element = document.getElementById(elementId);
-    element.classList.remove('hidden');
+// show function
+function show(elementId) {
+  const getElementId = document.querySelector(`#${elementId}`);
+  getElementId.classList.remove("hidden");
+}
+// random alphabet genaret function
+function randomAlphabetGenarate() {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz/";
+  const alphabetArr = alphabet.split("");
+  const randomInd = Math.floor(Math.random() * alphabetArr.length);
+  const randomAlphabetValue = alphabetArr[randomInd];
+  return randomAlphabetValue;
+}
+// set random alphabet value function
+function setAlphabetValue(AlphabetId, randomAlphabetValue) {
+  const currentAlphabetId = document.querySelector(`#${AlphabetId}`);
+  currentAlphabetId.innerText = randomAlphabetValue;
+  return currentAlphabetId;
+}
+// set current alphavet bg function
+function setCurrentAlphabetBg(elementId) {
+  const currentAlphabetIdBg = document.querySelector(`#${elementId}`);
+  currentAlphabetIdBg.classList.add("bg-yellow-500", "text-green-800");
+}
+// remove current alphavet bg function
+function removeCurrentAlphabetBg(elementId) {
+  const currentAlphabetIdBg = document.querySelector(`#${elementId}`);
+  currentAlphabetIdBg.classList.remove("bg-yellow-500", "text-green-800");
+}
+// score up function
+function scoreUp(scoreUpId) {
+  const scoreUpBtn = document.querySelector(`#${scoreUpId}`);
+  const scoreValue = scoreUpBtn.innerText;
+  const scoreNum = parseInt(scoreValue);
+  const scoreUp = scoreNum + 1;
+  const finalScore = (scoreUpBtn.innerText = scoreUp);
+  return finalScore;
 }
 
-function setBackgroundColorById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.add('bg-orange-400');
+// score down function
+function lifeDown(lifeDownId) {
+  const lifeDownBtn = document.getElementById(lifeDownId);
+  const lifeDownValue = lifeDownBtn.innerText;
+  const lifeDownNum = parseInt(lifeDownValue);
+  const lifeDown = lifeDownNum - 1;
+  const finalLifeDown = (lifeDownBtn.innerText = lifeDown);
+  if (finalLifeDown === 0) {
+    hide("play-ground");
+    show("score-screen");
+    const finalScoreValue = document.getElementById("score-up").innerText;
+    setFinalScore("final-score-value", finalScoreValue);
+  } else {
+    return finalLifeDown;
+  }
 }
 
-function removeBackgroundColorById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.remove('bg-orange-400');
+// set final score value function
+function setFinalScore(finalScoreId, setFinalScoreValue) {
+  const finalScoreBtn = document.getElementById(finalScoreId);
+  finalScoreBtn.innerText = setFinalScoreValue;
 }
-
-function getARandomAlphabet(){
-    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
-    const alphabets = alphabetString.split('');
-
-    const randomNumber = Math.random()*25;
-    const index = Math.round(randomNumber);
-
-    const alphabet = alphabets[index];
-    return alphabet;
-}
-
-// function getARandomAlphabet() {
-//     // get or create an alphabet array
-//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
-//     const alphabets = alphabetString.split('');
-//     // console.log(alphabets);
-
-//     // get a random index between 0 -25
-//     const randomNumber = Math.random() * 25;
-//     const index = Math.round(randomNumber);
-    
-//     const alphabet = alphabets[index];
-//     // console.log(index, alphabet);
-//     return alphabet;
-// }
